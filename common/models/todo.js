27e8e37430e3f80d58523c44f1049415a74a584b
Akira Sosa <akirasosa@gmail.com>
2014-12-08 18:31:30 +0700

    added index.html

--- common/models/todo.js
+++ common/models/todo.js
@@ -1,3 +1,10 @@
 module.exports = function(Todo) {
 
+  Todo.remoteMethod('destroyAll', {
+    description: 'Delete all matching records',
+    accepts: {arg: 'where', type: 'object', description: 'filter.where object'},
+    http: {verb: 'del', path: '/'},
+  });
+
 };
+
